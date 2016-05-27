30b5fddae76a1a14e60ca64370eeaf3c80104db5
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-05-27 11:06:49 +0200

    Add configurable port

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if [[ -z "$port" ]]; then
+	echo "port not set, using default"
+	port=5001
+fi
+
 if [ $server_int_net_floatingIp ]
 then
-	screen -d -m -S client iperf -c $server_int_net_floatingIp -t 300
+	screen -d -m -S client iperf -c $server_int_net_floatingIp -t 300 -p $port
 elif [ $server_softfire_test_floatingIp ]
 then
-	screen -d -m -S client iperf -c $server_softfire_test_floatingIp -t 300
+	screen -d -m -S client iperf -c $server_softfire_test_floatingIp -t 300 -p $port
 else
-	screen -d -m -S client iperf -c $server_private_floatingIp -t 300
+	screen -d -m -S client iperf -c $server_private_floatingIp -t 300 -p $port
 fi
