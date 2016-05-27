d986fa7f3fa012b95c2476148ada129d55a30a31
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-05-27 10:22:28 +0200

    Update server_configure_floatingIp.sh

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -3,9 +3,9 @@
 if [ $server_int_net_floatingIp ]
 then
 	screen -d -m -S client iperf -c $server_int_net_floatingIp -t 300
+elif [ $server_softfire_test_floatingIp ]
+then
+	screen -d -m -S client iperf -c $server_softfire_test_floatingIp -t 300
 else
-	if [ $server_softfire_test_floatingIp ]
-		screen -d -m -S client iperf -c $server_softfire_test_floatingIp -t 300
-	else
-		screen -d -m -S client iperf -c $server_private_floatingIp -t 300
+	screen -d -m -S client iperf -c $server_private_floatingIp -t 300
 fi
