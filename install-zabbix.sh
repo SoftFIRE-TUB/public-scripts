1cbc4b771f60e852bab66c0ee27c83ed0cb62d25
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-06-09 17:53:05 +0200

    Update install-zabbix.sh

--- install-zabbix.sh
+++ install-zabbix.sh
@@ -23,8 +23,8 @@ sudo apt-get update
 # avoid promt of pwd
 export DEBIAN_FRONTEND="noninteractive"
 
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password rootpw"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password rootpw"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 
 sudo apt-get install -y mysql-server-5.6
 
