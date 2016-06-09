dd5cb849180cb751cac631ee328ff818a3efd053
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-06-09 18:11:03 +0200

    Create zabbix_configure_agent.sh

---
+++ zabbix_configure_agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -z "$int_net_floatingIp" ]]; then
+  echo "int_net_floatingIp is required"
+  exit 1
+fi
+
+export MONITORING_IP=$int_net_floatingIp
+
+echo "Installing zabbix-agent for server at $MONITORING_IP"
+
+sudo apt-get install -y zabbix-agent
+sudo sed -i -e "s/ServerActive=127.0.0.1/ServerActive=$MONITORING_IP:10051/g" -e "s/Server=127.0.0.1/Server=$MONITORING_IP/g" -e "s/Hostname=Zabbix server/#Hostname=/g" /etc/zabbix/zabbix_agentd.conf
+sudo service zabbix-agent restart
+sudo rm zabbix-release_2.2-1+precise_all.deb
+echo "finished installing zabbix-agent!"
