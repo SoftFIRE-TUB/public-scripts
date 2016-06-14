ada08d7a6259e86c345d67d4b51780a52b84b467
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-06-14 11:36:11 +0200

    call the script

--- install-zabbix.sh
+++ install-zabbix.sh
@@ -61,4 +61,6 @@ sudo service apache2 restart
 sed -i -e "s/START=no/START=yes/g" /etc/default/zabbix-server
 sudo service zabbix-server restart
 
-mysql -u zabbix -p$zabbix_pwd zabbix -e "update actions set status = 0 where name like '%Auto discovery%';"
+# mysql -u zabbix -p$zabbix_pwd zabbix -e "update actions set status = 0 where name like '%Auto discovery%';"
+
+python zabbix-set-discovery.py
