742711babfa664f0d9727e67ac0fc3e806e7f961
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-06-27 15:04:59 +0200

    Update install-mongo.sh

--- install-mongo.sh
+++ install-mongo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+if [ ! -z "$http_proxy" ]; then
+  sudo apt-key adv --keyserver-options http-proxy=$http_proxy --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+else
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+fi
 
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
