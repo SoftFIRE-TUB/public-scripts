8ecc262d087a12179454e0c35c4d8ebb435404bb
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-06-27 15:42:38 +0200

    Update config_start-router.sh

--- config_start-router.sh
+++ config_start-router.sh
@@ -4,11 +4,11 @@ export LC_ALL=C
 
 screen -d -m -S router mongos --configdb $config_int_net_floatingIp:$port
 
-while ! nc -z localhost $port; do
-  sleep 0.5 # wait for 1/10 of the second before check again
-done
+#while ! nc -z localhost $port; do
+#  sleep 0.5 # wait for 1/10 of the second before check again
+#done
 
 echo "mongodb router is running"
 
 # just for security.....
-sleep 3
+sleep 7
