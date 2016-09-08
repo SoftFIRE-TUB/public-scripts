645e142221862c07d7b4c1afa830f1357dbdda88
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-09-08 10:33:57 +0200

    Update shard_configure.sh

--- shard_configure.sh
+++ shard_configure.sh
@@ -2,6 +2,6 @@
 
 export LC_ALL=C
 
-mongo --port $port --eval  "sh.addShard( '`echo $shard_softfire_test_floatingIp`:$shard_port' )"
+mongo --port $port --eval  "sh.addShard( '`echo $shard_softfire_internal_floatingIp:$shard_port`' )"
 
 mongo --port $port --eval  'sh.enableSharding("softfire")'
