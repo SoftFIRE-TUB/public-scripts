0161d58f1db1139ed2457163a22dd42525628ea4
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-09-08 10:35:56 +0200

    Update config_configure.sh

--- config_configure.sh
+++ config_configure.sh
@@ -2,7 +2,7 @@
 
 export LC_ALL=C
 
-screen -d -m -S router mongos --port $port --configdb $config_softfire_internal_floatingIp:$port
+screen -d -m -S router mongos --port $port --configdb $config_softfire_internal_floatingIp:$config_port
 
 #while ! nc -z localhost $port; do
 #  sleep 0.5 # wait for 1/10 of the second before check again
