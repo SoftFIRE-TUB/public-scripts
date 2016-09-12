ccdba0bcdcf0cb3357d2e86cc06a58d4da917130
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-09-12 10:37:12 +0200

    Update config_configure.sh

--- config_configure.sh
+++ config_configure.sh
@@ -4,8 +4,10 @@ export LC_ALL=C
 
 screen -d -m -S router mongos --port $port --configdb $config_softfire_internal_floatingIp:$config_port
 
+nproc=`sudo netstat -tulpn | grep $port | wc -l`
+
 COUNTER=1
-while ! nc -z localhost $port; do
+while [ $proc != 1 ]; do
   COUNTER=$[$COUNTER +1]
   sleep 0.5 # wait for 1/10 of the second before check again
   if [ $COUNTER == 250 ]; then
@@ -16,5 +18,5 @@ done
 
 echo "mongodb router is running"
 
-# just for security.....
+# .....
 sleep 2
