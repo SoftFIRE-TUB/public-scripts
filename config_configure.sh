80cc7ce7cce9624dc69122ee8e63af5841a33635
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-09-12 11:33:48 +0200

    Update config_configure.sh

--- config_configure.sh
+++ config_configure.sh
@@ -7,15 +7,15 @@ screen -d -m -S router mongos --port $port --configdb $config_softfire_internal_
 nproc=`sudo netstat -tulpn | grep $port | wc -l`
 
 COUNTER=1
-while [ $proc != 1 ]; do
-  COUNTER=$[$COUNTER +1]
-  sleep 0.5 # wait for 1/10 of the second before check again
-  if [ $COUNTER == 250 ]; then
+while [ 1 -eq "$nproc" ]; do
+  COUNTER=$(($COUNTER + 1))
+
+  sleep 5 # wait for 1/10 of the second before check again
+  if [ 100 -eq $COUNTER ]; then
     echo "mongodb not started"
     exit 99
   fi
 done
-
 echo "mongodb router is running"
 
 # .....
