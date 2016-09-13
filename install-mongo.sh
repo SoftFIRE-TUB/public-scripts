326a3aab369899ac1974049cdbd1195acbf59149
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2016-09-13 10:57:08 +0200

    Update install-mongo.sh

--- install-mongo.sh
+++ install-mongo.sh
@@ -9,6 +9,6 @@ fi
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
 sudo apt-get update
-sudo apt-get install -y mongodb-org screen
+sudo apt-get install -y --force-yes mongodb-org screen
 
 sudo service mongod stop
