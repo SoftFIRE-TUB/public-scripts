f67813fbaaf8fd5f2dfa7389ef46f0300bcd44b9
Björn <bjoern.riemer@web.de>
2017-03-14 16:15:10 +0100

    added softfire-internal floating ip to client

--- server_configure_floatingIp.sh
+++ server_configure_floatingIp.sh
@@ -7,10 +7,13 @@ fi
 
 if [ $server_int_net_floatingIp ]
 then
-	screen -d -m -S client iperf -c $server_int_net_floatingIp -t 36000 -p $port
+	screen -d -m -S client iperf -c $server_int_net_floatingIp -t 360 -p $port
 elif [ $server_softfire_test_floatingIp ]
 then
-	screen -d -m -S client iperf -c $server_softfire_test_floatingIp -t 36000 -p $port
+	screen -d -m -S client iperf -c $server_softfire_test_floatingIp -t 360 -p $port
+elif [ $server_softfire_internal_floatingIp ]
+then
+	screen -d -m -S client /bin/sh -c "iperf -c $server_softfire_internal_floatingIp -t 300; sh"
 else
-	screen -d -m -S client iperf -c $server_private_floatingIp -t 36000 -p $port
+	screen -d -m -S client iperf -c $server_private_floatingIp -t 360 -p $port
 fi
